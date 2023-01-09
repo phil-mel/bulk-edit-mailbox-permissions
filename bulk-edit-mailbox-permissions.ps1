23bca9ba999dff51b864f1b2900bf6929008170e
phil-mel <120044497+phil-mel@users.noreply.github.com>
2023-01-09 12:05:20 +0000

    Update bulk-edit-mailbox-permissions.ps1

--- bulk-edit-mailbox-permissions.ps1
+++ bulk-edit-mailbox-permissions.ps1
@@ -3,6 +3,7 @@
 #change [<365adm>] (line 6) to your 365 admin email address
 #works for adding or removing permissions, just change "Add-" to "Remove-" as needed
 #comment out or delete mailboxpermission or recipientpermission line if only one is needed
+#script will prompt you to input the name of the mailbox you are editing
 
 Connect-ExchangeOnline -UserPrincipalName [<>]
 
